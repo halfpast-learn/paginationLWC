fd513126842358d734a6ae919fbdf318010ad00a
Dmitry Kovalev <dmitry.kovalev@customertimes.com>
2022-03-06 14:46:18 +0300

    add pagination

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,9 +1,74 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountLoader.getAccountList';
+import findAccounts from '@salesforce/apex/accountLoader.findAccounts';
 
 export default class AccountList extends LightningElement {
-    @wire(getAccountList) accs;
+    pageNumber;
+    recordAmount;
+    prevPageNumber;
+    nextPageNumber;
+    totalPages;
+    isFiltered;
+    filterString;
+    @track accs;
+    @wire(getAccountList) wiredData({ error, data }) {
+        if (data) {
+            this.accs = data;
+            this.totalPages = this.accs.length / this.recordAmount;
+            this.error = undefined;
+        }
+        else if (error) {
+            this.error = error;
+            this.accs = undefined;
+        }
+    }
+    handleSearch(event) {
+        this.filterString = event.target.value;
+        if (this.filterString != '') {
+            this.isFiltered = true;
+        }
+    }
+    //todo: 
+    //sosl to table?
+    //split the code. accpage should only return page
+    get accPage() {
+        if (!this.isFiltered) {
+            return this.accs.slice((this.pageNumber - 1) * this.recordAmount, this.pageNumber * this.recordAmount);
+        }
+        else if (this.isFiltered) {
+            let result = [];
+            for (const record in this.accs) {
+                /*if (record.name.includes(this.filterString)) {
+                    result.push(record);
+                }*/
+            }
+            console.log(this.accs);
+            //return result;
+        }
+    }
+
+    handlePageChange(event) {
+        let dif = 0;
+        switch (event.target.label) {
+            case "Next":
+                dif = 1;
+                break;
+            case "Previous":
+                dif = -1;
+                break;
+            case ">>":
+                dif = this.totalPages - this.pageNumber;
+                break;
+            case "<<":
+                dif = -(this.pageNumber - 1);
+                break;
+        }
+        if (this.pageNumber + dif >= 1 && this.pageNumber + dif <= this.totalPages)
+            this.pageNumber += dif;
+    }
+
     connectedCallback() {
-        console.log(this.accs.data);
+        this.pageNumber = 1;
+        this.recordAmount = 5;
     }
 }
