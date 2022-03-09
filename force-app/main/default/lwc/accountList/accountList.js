43baa88e222e43694617226ec651a00f826864bd
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 17:33:46 +0300

    pagination is server side

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,7 +1,26 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountLoader.getAccountList';
-import findAccounts from '@salesforce/apex/accountLoader.findAccounts';
+import getTotalPages from '@salesforce/apex/accountLoader.getTotalPages';
 
 export default class AccountList extends LightningElement {
-    
+    @track accs;
+    @track totalPages;
+    handlePageRequest(event){
+        let page = event.detail.page-1;
+        let recordAmount = event.detail.limit;
+        console.log("page requested: ", page, recordAmount);
+        getAccountList({lim:recordAmount,offset:page*recordAmount})
+            .then(result => {
+                this.accs=result;
+                console.log(this.accs);
+            });
+    }
+    connectedCallback()
+    {
+        getTotalPages()
+            .then(result=> {
+                this.totalPages=result/5;
+                console.log(this.totalPages);
+            });
+    }
 }
--- force-app/main/default/lwc/accountTableView/accountTableView.js
+++ force-app/main/default/lwc/accountTableView/accountTableView.js
@@ -1,64 +1,47 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountLoader.getAccountList';
 import findAccounts from '@salesforce/apex/accountLoader.findAccounts';
 
 export default class AccountTableView extends LightningElement {
-    pageNumber;
+    currentPage;
     recordAmount;
-    prevPageNumber;
-    nextPageNumber;
     totalPages;
-    isFiltered;
-    filterString;
-    @track accs;
-    @wire(getAccountList) wiredData({ error, data }) {
-        if (data) {
-            this.accs = data;
-            this.totalPages = this.accs.length / this.recordAmount;
-            this.error = undefined;
-        }
-        else if (error) {
-            this.error = error;
-            this.accs = undefined;
-        }
-    }
-    handleSearch(event) {
-        this.filterString = event.target.value;
-        if (this.filterString != '') {
-            this.isFiltered = true;
-        }
-    }
-    //todo: 
-    //sosl to table?
-    //split the code. accpage should only return page
+    @api currentAccs;
     get accPage() {
-        if (!this.isFiltered) {
-            return this.accs.slice((this.pageNumber - 1) * this.recordAmount, this.pageNumber * this.recordAmount);
-        }
+        return this.currentAccs;
     }
 
     handlePageChange(event) {
-        let dif = 0;
+        let pageToRequest;
         switch (event.target.label) {
             case "Next":
-                dif = 1;
+                pageToRequest = this.currentPage+1;
                 break;
             case "Previous":
-                dif = -1;
+                pageToRequest = this.currentPage-1;
                 break;
             case ">>":
-                dif = this.totalPages - this.pageNumber;
+                pageToRequest=this.totalPages;
                 break;
             case "<<":
-                dif = -(this.pageNumber - 1);
+                pageToRequest=1;
                 break;
         }
-        if (this.pageNumber + dif >= 1 && this.pageNumber + dif <= this.totalPages)
-            this.pageNumber += dif;
+        //add totalpages comparison
+        if (pageToRequest>0 && pageToRequest!=this.currentPage)
+        {
+            this.requestPage(pageToRequest, this.recordAmount);
+            this.currentPage=pageToRequest;
+        }
     }
-
+    requestPage(pageToRequest, recordAmount)
+    {
+        const requestEvent = new CustomEvent("pagerequest", {detail:{page:pageToRequest,limit:recordAmount}});
+        this.dispatchEvent(requestEvent);
+    }
     connectedCallback() {
-        this.pageNumber = 1;
+        this.currentPage = 1;
         this.recordAmount = 5;
+        this.requestPage(this.currentPage,this.recordAmount);
     }
 }
